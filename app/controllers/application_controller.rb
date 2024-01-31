b68caae2ee08aa5e87020fd54c5a089ad3829d90
firstpj <114886262+firstpj@users.noreply.github.com>
2024-01-31 11:51:57 +0100

    add app/controllers/application_controller.rb01, app/models/user.rb, app/controllers/application_controller.rb01, db/migrate/20240131100020_add_devise_to_users.rb files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
-# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-  # Your code here
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, address_attributes: [:country, :state, :city, :area, :postal_code]])
+  end
 end
