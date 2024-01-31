9f95df6f27d91f7ab0870b3639b63d2dbdb9bb67
firstpj <114886262+firstpj@users.noreply.github.com>
2024-01-31 20:47:18 +0100

    add Gemfile, Gemfile lock,  app/models/ability.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_31_205704) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_31_145241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_31_205704) do
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
   end
 
   add_foreign_key "comments", "posts"
