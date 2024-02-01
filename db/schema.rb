69eb3dc6b5cacfcfd07feb392c257e0bfe39ad71
firstpj <akpocollins2005@gmail.com>
2024-02-02 00:09:47 +0100

    Fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_01_083348) do
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "posts_count"
+    t.string "role"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
